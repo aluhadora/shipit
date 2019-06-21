aedadd09204d8f4268262ac4ce88efa93159eb5a
Allan Harper <allan.harper@progleasing.com>
2019-06-21 08:54:03 -0600

    More Leaderboardsg

--- src/Components/Leaderboards/leaderboards.jsx
+++ src/Components/Leaderboards/leaderboards.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Track from '../track';
 import PopularSongs from './popular_songs';
+import RepeatedSongs from './repeated_songs';
 
 //https://shipitback20190620080140.azurewebsites.net/api/mostpopularsong
 class Leaderboards extends Component {
-    state = { popularSong: {}, page: null }
+    state = { popularSong: {}, repeatedSong: {}, page: null }
 
     componentDidMount() {
         fetch("https://shipitback20190620080140.azurewebsites.net/api/mostpopularsong")
@@ -13,6 +14,20 @@ class Leaderboards extends Component {
                 this.setState(function () {
                 return {
                     popularSong: obj[0]
+                };
+                });
+                this.forceUpdate();
+            }, 'aluhadora');
+
+
+
+
+        fetch("https://shipitback20190620080140.azurewebsites.net/api/values")
+            .then(res => res.json())
+            .then(obj => {
+                this.setState(function () {
+                return {
+                    repeatedSong: obj[0]
                 };
                 });
                 this.forceUpdate();
@@ -26,9 +41,16 @@ class Leaderboards extends Component {
         })
     }
 
+    pageToRepeated()
+    {
+        this.setState({
+            page: <RepeatedSongs />
+        })
+    }
+
 
     render() { 
-        const { popularSong, page } = this.state;
+        const { popularSong, page, repeatedSong } = this.state;
 
         return ( page || 
             <div style={{marginTop: 50}}>
@@ -39,6 +61,11 @@ class Leaderboards extends Component {
                         img: popularSong.image, 
                         info: "Has been played " + popularSong.songCount + " times",
                         link: {text: "More popular songs...", onClick: this.pageToPopular.bind(this)}}}/>
+
+                    <Track track={{name: repeatedSong.songName, 
+                        img: repeatedSong.image, 
+                        info: "Has been played " + repeatedSong.songCount + " times by " + repeatedSong.name + " alone",
+                        link: {text: "More repeated songs...", onClick: this.pageToRepeated.bind(this)}}}/>
                 </div>
                 
 
