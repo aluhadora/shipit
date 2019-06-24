b95ba6a2f01dd126a77aa3d3b0489eb0448a3f81
Allan Harper <allan.harper@progleasing.com>
2019-06-24 16:01:14 -0600

    Testing routing

--- src/Components/listen.jsx
+++ src/Components/listen.jsx
@@ -3,6 +3,7 @@ import Menu from './menu';
 import ActivityBar from './activity_bar';
 import Leaderboards from './Leaderboards/leaderboards';
 import * as Actions from '../Actions/api_actions';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Listen extends Component {
     state = { user: null }
@@ -21,7 +22,11 @@ class Listen extends Component {
 
         return <div>
             <Menu user={user} />
-            <Leaderboards user={user} />
+            <Router>
+                <Route exact path="/" render={() => <Leaderboards user={user} />}/>
+            </Router>
+            
+            {/* <Leaderboards user={user} /> */}
             <ActivityBar user={user} />
         </div>;
     }
